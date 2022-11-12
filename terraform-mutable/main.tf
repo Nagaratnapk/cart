188300f20bcc06a8260c4d6841d979ab302298ff
Nagaratna Karetimmannanavar <nagaratnakaretimmannanavar@Nagaratnas-MBP.attlocal.net>
2022-11-11 19:02:43 -0800

    update

--- terraform-mutable/main.tf
+++ terraform-mutable/main.tf
@@ -6,6 +6,8 @@ module "cart" {
   SPOT_INSTANCE_COUNT  = var.SPOT_INSTANCE_COUNT
   OD_INSTANCE_COUNT    = var.OD_INSTANCE_COUNT 
   APP_PORT             = var.APP_PORT
+  APP_VERSION          = var.APP_VERSION
+  LB_TYPE              = "internal"
 }
 
 
